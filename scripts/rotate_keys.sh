6ff305f81000af81919ccc7da282f52b3013a8c0
gmariakakis <gmariakakis@gmail.com>
2025-07-26 03:06:45 +0300

    fix rotate script and upload limit

--- scripts/rotate_keys.sh
+++ scripts/rotate_keys.sh
@@ -6,4 +6,5 @@ IFS='
 
 # Rotate Matrix Synapse signing keys
 
-docker exec synapse python -m synapse.app.rotate_signing_keys -c /data/homeserver.yaml
+cd /opt/chat
+docker compose exec synapse python -m synapse.app.rotate_signing_keys -c /data/homeserver.yaml
